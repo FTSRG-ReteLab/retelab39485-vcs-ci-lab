4aa687fa712674ba7f50c49b73e7ce931c9f8fb8
bence <gyongyossy0122@gmail.com>
2018-02-26 07:06:40 -0800

    Moved JUnit test to proper folder

---
+++ train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorImplTest.java
@@ -0,0 +1,14 @@
+package hu.bme.mit.train.sensor;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TrainSensorImplTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
